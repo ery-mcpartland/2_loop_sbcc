311c70519d3f16fafa7f6184d76db0fb86c66bd9
Ery McPartland <ery.mcpartland@gmail.com>
2025-02-12 17:55:30 +0000

    Write all tensor reduction functions, untested

--- Functions/ScalarIntegrals.m
+++ Functions/ScalarIntegrals.m
@@ -57,7 +57,29 @@ LScalarQ[q2] = True;
 (*Tensor Reductions*)
 
 
-TensorReduction["a",int_]:=int/.{r->P1 p,l->P2 q}
+TensorReduction["a",int_]:=int/.{
+l->(-(((-3 mc^2-3 P2+2 P7+P9-q2) q2+mb^2 (mc^2+P2-P9+q2)) p)+((-2 mc^2-2 P2+P7+P9-q2) q2+mb^2 (P7-P9+q2)) q)/t^2,
+r->(-((mb^4+mb^2 (P5-P8-q2)+(-2 P4+P5+P8) q2) Subscript[p, \[Nu]])+(mb^4+(-P4+P8) q2-mb^2 (P4-2 P5+P8+q2)) Subscript[q, \[Nu]])/t^2}
+
+
+TensorReduction["b",int_]:=int/.{
+l->(-(((-3 mc^2-3 P2+2 P7+P9-q2) q2+mb^2 (mc^2+P2-P9+q2)) p)+((-2 mc^2-2 P2+P7+P9-q2) q2+mb^2 (P7-P9+q2)) q)/t^2,
+r->(-((mb^4+(P10+P11-2 P4) q2+mb^2 (P10-P11+q2)) p)+(2 mb^4+mb^2 (2 P10-P11-P4)+(P11-P4) q2) q)/t^2}
+
+
+TensorReduction["c",int_]:=int/.{
+l->((mb^2 (P1-P2-q2)+q2 (2 mc^2-P1+3 P2-2 P7+q2)) p-(mb^2 (mc^2+P1-P7-q2)+q2 (mc^2-P1+2 P2-P7+q2)) q)/t^2,
+r->((-mb^2 (P4-P6+q2)+q2 (3 P4-2 P5-P6+q2)) p+((-2 P4+P5+P6-q2) q2+mb^2 (P5-P6+q2)) q)/t^2}
+
+
+TensorReduction["d",int_]:=int/.{
+l->((mb^2 (P1-P2-q2)+q2 (2 mc^2-P1+3 P2-2 P7+q2)) p-(mb^2 (mc^2+P1-P7-q2)+q2 (mc^2-P1+2 P2-P7+q2)) q)/t^2,
+r->(-((q2 (2 P11-P4-P6+q2)+mb^2 (P4-P6+q2)) p)+((P11-P4) q2+mb^2 (P11+P4-2 P6+2 q2)) q)/t^2}
+
+
+TensorReduction["e",int_]:=int/.{
+l->((mb^2 (P1-P2-q2)+q2 (2 mc^2-P1+3 P2-2 P7+q2)) p-(mb^2 (mc^2+P1-P7-q2)+q2 (mc^2-P1+2 P2-P7+q2)) q)/t^2,
+r->1/t^2 ((mb^2 (-P1+P12+P2-P3)+(P1-P12-2 P13-P2+P3+2 P4) q2) Subscript[p, r]+(mb^2 (P1-P12+P13-P2+P3-P4)+(-P1+P12+P13+P2-P3-P4) q2) q)}
 
 
 ReduceToScalar[i_,int_]:=Simplify[FermionLineExpand[TensorReduction[i,int],
