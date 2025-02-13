205d843b647ccc66139d63815fe5342685581cab
Ery McPartland <ery.mcpartland@gmail.com>
2025-02-13 16:50:00 +0000

    Add t as a known scalar, results now satisfy closure test

--- Functions/ScalarIntegrals.m
+++ Functions/ScalarIntegrals.m
@@ -13,11 +13,19 @@
 
 
 (* ::Subsection:: *)
-(*Tensor Reductions*)
+(*Tensor Reduction*)
 
 
 (* ::Text:: *)
-(*Five functions, one for each class of diagram. Takes a tensor integrand with all scalar products written in terms of P_i, mc, mb and q2 and converts to a scalar integrand using Passarino-Veltman. Takes two arguments , type of diagram ("a" to "e") and tensor integral to reduce. *)
+(*Five functions, one for each class of diagram.  Contains the Passarion-Veltman rules for each diagram family. Takes a tensor integrand with all scalar products written in terms of P_i, mc, mb and q2 and converts to a scalar integrand using Passarino-Veltman. Takes two arguments , type of diagram ("a" to "e") and tensor integral to reduce. *)
+
+
+(* ::Subsection:: *)
+(*Reduce to Scalar*)
+
+
+(* ::Text:: *)
+(*One function, takes two arguments - diagram family and tensor integral to reduce. Applies Passarino-Veltman replacement rules performs any remaining dirac algebra and applies and equations of motion to get final result.*)
 
 
 (* ::Section:: *)
@@ -51,6 +59,7 @@ LScalarQ[P13] = True;
 LScalarQ[mc] = True;
 LScalarQ[mb] = True;
 LScalarQ[q2] = True;
+LScalarQ[t] = True;
 
 
 (* ::Subsection:: *)
@@ -59,7 +68,7 @@ LScalarQ[q2] = True;
 
 TensorReduction["a",int_]:=int/.{
 l->(-(((-3 mc^2-3 P2+2 P7+P9-q2) q2+mb^2 (mc^2+P2-P9+q2)) p)+((-2 mc^2-2 P2+P7+P9-q2) q2+mb^2 (P7-P9+q2)) q)/t^2,
-r->(-((mb^4+mb^2 (P5-P8-q2)+(-2 P4+P5+P8) q2) Subscript[p, \[Nu]])+(mb^4+(-P4+P8) q2-mb^2 (P4-2 P5+P8+q2)) Subscript[q, \[Nu]])/t^2}
+r->(-((mb^4+mb^2 (P5-P8-q2)+(-2 P4+P5+P8) q2)p)+(mb^4+(-P4+P8) q2-mb^2 (P4-2 P5+P8+q2)) q)/t^2}
 
 
 TensorReduction["b",int_]:=int/.{
@@ -79,8 +88,12 @@ r->(-((q2 (2 P11-P4-P6+q2)+mb^2 (P4-P6+q2)) p)+((P11-P4) q2+mb^2 (P11+P4-2 P6+2 
 
 TensorReduction["e",int_]:=int/.{
 l->((mb^2 (P1-P2-q2)+q2 (2 mc^2-P1+3 P2-2 P7+q2)) p-(mb^2 (mc^2+P1-P7-q2)+q2 (mc^2-P1+2 P2-P7+q2)) q)/t^2,
-r->1/t^2 ((mb^2 (-P1+P12+P2-P3)+(P1-P12-2 P13-P2+P3+2 P4) q2) Subscript[p, r]+(mb^2 (P1-P12+P13-P2+P3-P4)+(-P1+P12+P13+P2-P3-P4) q2) q)}
+r->1/t^2 ((mb^2 (-P1+P12+P2-P3)+(P1-P12-2 P13-P2+P3+2 P4) q2)p+(mb^2 (P1-P12+P13-P2+P3-P4)+(-P1+P12+P13+P2-P3-P4) q2) q)}
 
 
-ReduceToScalar[i_,int_]:=Simplify[FermionLineExpand[TensorReduction[i,int],
-ChisholmExpand->False,GordonIdentity->False]]
+(* ::Subsection:: *)
+(*Reduce to Scalar*)
+
+
+ReduceToScalar[i_,int_]:=Simplify[FermionLineExpand[TensorReduction[i,int],ChisholmExpand->False,
+GordonIdentity->False]/.{LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}]
