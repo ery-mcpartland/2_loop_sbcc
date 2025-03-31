6988000e11cec657d7bdbc1fd448f160d5440503
Ery McPartland <ery.mcpartland@gmail.com>
2025-03-31 11:46:08 +0100

    Add functions for all quark massess

--- Functions/TensorIntegrals.m
+++ Functions/TensorIntegrals.m
@@ -40,8 +40,28 @@ LDot[l,p]->1/2 (-2 mc^2-2 P2+P7+P9-q2),LDot[l,q]->1/2 (-mc^2-P2+P9-q2),LDot[r,r]
 LDot[q,r]->1/2 (-mb^2-P5+P8),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
 
 
+PropagatorReplace["ahat",amp_]:=amp /. {LDot[l,l]->P2hat,LDot[l,r]->1/2 (-P2hat+P3hat-P4),LDot[l,p]->1/2 (-2 P2hat+P7+P9-q2),
+LDot[l,q]->1/2 (-P2hat+P9-q2),LDot[r,r]->P4,LDot[p,r]->1/2 (-mb^2-P4+P8),LDot[q,r]->1/2 (-mb^2-P5+P8),LDot[p,p]->mb^2,
+LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
+
+
+PropagatorReplace["amb",amp_]:=amp /. {LDot[l,l]->mb^2+P2mb,LDot[l,r]->1/2 (-P2mb+P3mb-P4),
+LDot[l,p]->1/2 (-2 mb^2-2 P2mb+P7+P9-q2),LDot[l,q]->1/2 (-mb^2-P2mb+P9-q2),LDot[r,r]->P4,LDot[p,r]->1/2 (-mb^2-P4+P8),
+LDot[q,r]->1/2 (-mb^2-P5+P8),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
+
+
 PropagatorReplace["b",amp_]:=amp /. {LDot[l,l]->mc^2+P2,LDot[l,r]->1/2 (-P2+P3-P4),
 LDot[l,p]->1/2 (-2 mc^2-2 P2+P7+P9-q2),LDot[l,q]->1/2 (-mc^2-P2+P9-q2),LDot[r,r]->P4,LDot[p,r]->(P11-P4)/2,
+LDot[q,r]->1/2 (-mb^2-P10+P11),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
+
+
+PropagatorReplace["bhat",amp_]:=amp /. {LDot[l,l]->P2hat,LDot[l,r]->1/2 (-P2hat+P3hat-P4),LDot[l,p]->1/2 (-2 P2hat+P7+P9-q2),
+LDot[l,q]->1/2 (-P2hat+P9-q2),LDot[r,r]->P4,LDot[p,r]->(P11-P4)/2,LDot[q,r]->1/2 (-mb^2-P10+P11),LDot[p,p]->mb^2,
+LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
+
+
+PropagatorReplace["bmb",amp_]:=amp /. {LDot[l,l]->mb^2+P2mb,LDot[l,r]->1/2 (-P2mb+P3mb-P4),
+LDot[l,p]->1/2 (-2 mb^2-2 P2mb+P7+P9-q2),LDot[l,q]->1/2 (-mb^2-P2mb+P9-q2),LDot[r,r]->P4,LDot[p,r]->(P11-P4)/2,
 LDot[q,r]->1/2 (-mb^2-P10+P11),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
 
 
@@ -50,11 +70,42 @@ LDot[l,p]->1/2 (-mc^2+P1-2 P2+P7-q2),LDot[l,q]->1/2 (P1-P2-q2),LDot[r,r]->P4,LDo
 LDot[q,r]->1/2 (-P4+P6-q2),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
 
 
+PropagatorReplace["chat",amp_]:=amp /. {LDot[l,l]->P2hat,LDot[l,r]->1/2 (-P2hat+P3hat-P4),
+LDot[l,p]->1/2 (P1hat-2 P2hat+P7-q2),LDot[l,q]->1/2 (P1hat-P2hat-q2),LDot[r,r]->P4,LDot[p,r]->1/2 (-2 P4+P5+P6-q2),
+LDot[q,r]->1/2 (-P4+P6-q2),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
+
+
+PropagatorReplace["cmb",amp_]:=amp /. {LDot[l,l]->mb^2+P2mb,LDot[l,r]->1/2 (-P2mb+P3mb-P4),
+LDot[l,p]->1/2 (-mb^2+P1mb-2 P2mb+P7-q2),LDot[l,q]->1/2 (P1mb-P2mb-q2),LDot[r,r]->P4,LDot[p,r]->1/2 (-2 P4+P5+P6-q2),
+LDot[q,r]->1/2 (-P4+P6-q2),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
+
+
 PropagatorReplace["d",amp_]:=amp /.{LDot[l,l]->mc^2+P2,LDot[l,r]->1/2 (-P1+P12-P6+q2),
 LDot[l,p]->1/2 (-mc^2+P1-2 P2+P7-q2),LDot[l,q]->1/2 (P1-P2-q2),LDot[r,r]->P4,LDot[p,r]->(P11-P4)/2,
 LDot[q,r]->1/2 (-P4+P6-q2),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2} 
 
 
-PropagatorReplace["e",amp_]:=amp /.{LDot[l,l]->mc^2+P2,LDot[l,r]->1/2 (-P2+P3-P4),
-LDot[l,p]->1/2 (-mc^2+P1-2 P2+P7-q2),LDot[l,q]->1/2 (P1-P2-q2),LDot[r,r]->P4,LDot[p,r]->1/2 (-P1+P12+P13+P2-P3-P4),
-LDot[q,r]->1/2 (-P1+P12+P2-P3),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
+PropagatorReplace["dhat",amp_]:=amp /.{LDot[l,l]->P2hat,LDot[l,r]->1/2 (P12hat-P1hat-P6+q2),
+LDot[l,p]->1/2 (P1hat-2 P2hat+P7-q2),LDot[l,q]->1/2 (P1hat-P2hat-q2),LDot[r,r]->P4,LDot[p,r]->(P11-P4)/2,
+LDot[q,r]->1/2 (-P4+P6-q2),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
+
+
+PropagatorReplace["dmb",amp_]:=amp /.{LDot[l,l]->mb^2+P2mb,LDot[l,r]->1/2 (P12mb-P1mb-P6+q2),
+LDot[l,p]->1/2 (-mb^2+P1mb-2 P2mb+P7-q2),LDot[l,q]->1/2 (P1mb-P2mb-q2),LDot[r,r]->P4,LDot[p,r]->(P11-P4)/2,
+LDot[q,r]->1/2 (-P4+P6-q2),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
+
+
+PropagatorReplace["e",amp_]:=amp /.{LDot[l,l]->mc^2+P2,LDot[l,r]->1/2(-P2+P3-P4),LDot[l,p]->1/2(-mc^2+P1-2P2+P7-q2), 
+LDot[l,q]->1/2(P1-P2-q2),LDot[r,r]->P4,LDot[p,r]->1/2(-P1+P12+P13+P2-P3-P4),LDot[q,r]->1/2(-P1+P12+P2-P3),
+LDot[p,p]->mb^2,LDot[p,q]->1/2(mb^2+q2),LDot[q,q]->q2}
+
+
+PropagatorReplace["ehat",amp_]:=amp /.{LDot[l,l]->P2hat,LDot[l,r]->1/2 (-P2hat+P3hat-P4),LDot[l,p]->1/2 (P1hat-2 P2hat+P7-q2),
+LDot[l,q]->1/2 (P1hat-P2hat-q2),LDot[r,r]->P4,LDot[p,r]->1/2 (P12hat+P13-P1hat+P2hat-P3hat-P4),
+LDot[q,r]->1/2 (P12hat-P1hat+P2hat-P3hat),LDot[p,p]->mb^2,LDot[p,q]->1/2 (mb^2+q2),LDot[q,q]->q2}
+
+
+PropagatorReplace["emb",amp_]:=amp /.{LDot[l,l]->mb^2+P2mb,LDot[l,r]->1/2(-P2mb+P3mb-P4),
+LDot[l,p]->1/2(-mb^2+P1mb-2P2mb+P7-q2),LDot[l,q]->1/2(P1mb-P2mb-q2),LDot[r,r]->P4,
+LDot[p,r]->1/2(P12mb+P13-P1mb+P2mb-P3mb-P4),LDot[q,r]->1/2(P12mb-P1mb+P2mb-P3mb),LDot[p,p]->mb^2,LDot[p,q]->1/2(mb^2+q2),
+LDot[q,q]->q2}
